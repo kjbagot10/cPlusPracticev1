13afe8ce729b68ebbc50cbe8eea3e4f15f11e901
Kieran Bagot <kjbagot10@gmail.com>
2024-06-03 17:48:00 +0100

    swap two variables

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+using namespace std; // removes need to put std etc;
+
 int main() 
 {
-	std::cout << "Hello, World" << std::endl;
+	int a = 1;
+	int b =2 ;
+	int a_prev = a;
+	a = b;
+	b = a_prev;
+	
+	cout << a;
+	cout << b;
 	return 0;
 }
 
