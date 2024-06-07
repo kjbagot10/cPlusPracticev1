8027920a4bc1f8a14b2757888f9f1ab0dffac40d
Kieran Bagot <kjbagot10@gmail.com>
2024-06-07 18:49:55 +0100

    no servo jitter

--- firstServo.cpp
+++ firstServo.cpp
@@ -1,10 +1,15 @@
 #include <pigpio.h>
 #include <iostream>
 
+
 using namespace std;
 
 int main()
 {
+	static unsigned int INI_PIN = 1; // one that controls everything
+	
 	cout << "Hello World";
 	return 0;
 }
+
+
---
+++ servoWithFactory.py
@@ -0,0 +1,26 @@
+from gpiozero import AngularServo
+from time import sleep
+from gpiozero import Servo
+
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory() # stops jitter. the first time it ran it must use sudo pigpiod
+
+servo = Servo(18, pin_factory=factory)
+
+#servoAngular = AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory);
+# 
+# while True:
+# 	servoAngular.angle = 0; 
+# 	
+# 	servoAngular.angle = 180;
+# 	sleep(2);
+# 	servoAngular.angle = 270;
+# 	sleep(2);
+while True:
+    servo.mid()
+    sleep(2)
+    servo.min()
+    sleep(2)
+    servo.max()
+    sleep(2)
