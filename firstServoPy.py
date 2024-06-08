2b79d2a8a0c207bb6ee5f0ecc21b969166573be0
Kieran Bagot <kjbagot10@gmail.com>
2024-06-08 10:44:56 +0100

    Some playing around with the new thing

--- firstServoPy.py
+++ firstServoPy.py
@@ -4,7 +4,7 @@ from time import sleep
 servo = AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0006, max_pulse_width=0.0023);
 
 while True:
-	servo.angle = 0;
+	servo.angle = 0; 
 	sleep(2)
 	servo.angle = 140;
 	sleep(2);
--- servoWithFactory.py
+++ servoWithFactory.py
@@ -6,21 +6,29 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 
 factory = PiGPIOFactory() # stops jitter. the first time it ran it must use sudo pigpiod
 
-servo = Servo(18, pin_factory=factory)
+# servo = Servo(18, pin_factory=factory)
 
-#servoAngular = AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory);
-# 
-# while True:
-# 	servoAngular.angle = 0; 
-# 	
-# 	servoAngular.angle = 180;
-# 	sleep(2);
-# 	servoAngular.angle = 270;
-# 	sleep(2);
+
+
+servoAngular = AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory);
+ 
 while True:
-    servo.mid()
-    sleep(2)
-    servo.min()
-    sleep(2)
-    servo.max()
-    sleep(2)
+    servoAngular.angle = 0;
+    sleep(0.5)
+    servoAngular.angle = 90;
+    sleep(0.5);
+#     servoAngular.angle = 270;
+#     sleep(0.5);
+# while True:
+#     servo.mid()
+#     sleep(2)
+#     servo.min()
+#     sleep(2)
+#     servo.max()
+#     sleep(2)
+
+#servo.max()
+#sleep(5)
+#servo.min()
+#sleep(5)
+#servo.max()
